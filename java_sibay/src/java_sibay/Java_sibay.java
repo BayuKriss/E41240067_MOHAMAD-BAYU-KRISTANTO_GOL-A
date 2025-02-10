55136f35c472d2964c3b8ad69363ec34573806dc
Yaiya0206 <naufalyafi0206@gmail.com>
2025-02-11 06:22:54 +0700

    First Commit

---
+++ java_sibay/src/java_sibay/Java_sibay.java
@@ -0,0 +1,13 @@
+
+package java_sibay;
+
+public class Java_sibay {
+
+    public static void main(String[] args) {
+     int a = 10;
+     int b = 20;
+     int c= b/a;
+        System.out.println(c);
+    }
+    
+}
